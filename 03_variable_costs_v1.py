ed9b549cf0fcef910222c262edca452434856610
hoggc0044 <125226354+hoggc0044@users.noreply.github.com>
2023-03-24 12:21:24 +1300

    Add files via upload

--- 03_variable_costs_v1.py
+++ 03_variable_costs_v1.py
@@ -2,10 +2,22 @@ import pandas
 
 
 def num_check(question, error, num_type):
+    valid = False
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
 
 
 def not_blank(question, error):
-
     valid = False
     while not valid:
         response = input(question)
@@ -40,7 +52,7 @@ variable_dict = {
 product_name = not_blank("Product name: ", "The product name can't be blank.")
 
 # loop to get component, quantity and price
-item_name - ""
+item_name = ""
 
 while item_name.lower() != "xxx":
 
@@ -56,19 +68,17 @@ while item_name.lower() != "xxx":
 
     price = num_check("How much for a single item? $", "The price must be a number <more than 0>", float)
 
-
 # add item, quantity and price to lists
 item_list.append(item_name)
 quantity_list.append(quantity)
 price_list.append(price)
 
-
 variable_frame = pandas.DataFrame(variable_dict)
 variable_frame = variable_frame.set_index('Item')
 
 # Calculate the cost of each component
-variable_frame['Cost'] = variable_frame['Quantity']\
-    * variable_frame['Price']
+variable_frame['Cost'] = variable_frame['Quantity'] \
+                         * variable_frame['Price']
 
 # Find sub-total
 variable_sub = variable_frame['Cost'].sum()
--- 03_variable_costs_v2.py
+++ 03_variable_costs_v2.py
@@ -1,8 +1,5 @@
-# import libraries
 import pandas
 
-
-# Functions start here
 
 def num_check(question, error, num_type):
     valid = False
@@ -20,26 +17,6 @@ def num_check(question, error, num_type):
             print(error)
 
 
-# Checks that the user has entered yes / no to a question
-def yes_no(question):
-
-    to_check = ["yes", "no"]
-
-    valid = False
-    while not valid:
-
-        response = input(question).lower()
-
-        for var_item in to_check:
-            if response == var_item:
-                return response
-            elif response == var_item[0]:
-                return var_item
-
-        print("Please enter either yes or no...\n")
-
-
-# Ensures that the string isn't blank
 def not_blank(question, error):
     valid = False
     while not valid:
@@ -66,7 +43,7 @@ def get_expenses(var_fixed):
     quantity_list = []
     price_list = []
 
-    expense_dict = {
+    variable_dict = {
         "Item": item_list,
         "Quantity": quantity_list,
         "Price": price_list
@@ -86,12 +63,7 @@ def get_expenses(var_fixed):
         if item_name.lower() == "xxx":
             break
 
-        if var_fixed == "variable":
-            quantity = num_check("Quantity:", "The amount must be a whole number which is more than zero", int)
-
-        else:
-            quantity = 1
-
+        quantity = num_check("Quantity:", "The amount must be a whole number which is more than zero", int)
         price = num_check("How much for a single item? $", "The price must be a number <more than 0>", float)
 
         # add item, quantity and price to lists
@@ -99,7 +71,7 @@ def get_expenses(var_fixed):
         quantity_list.append(quantity)
         price_list.append(price)
 
-        expense_frame = pandas.DataFrame(expense_dict)
+        expense_frame = pandas.DataFrame(variable_dict)
         expense_frame = expense_frame.set_index('Item')
 
         expense_frame['Cost'] = expense_frame['Quantity'] * expense_frame['Price']
@@ -110,17 +82,7 @@ def get_expenses(var_fixed):
         for item in add_dollars:
             expense_frame[item] = expense_frame[item].apply(currency)
 
-    return [expense_frame, expense_sub]
-
-
-# Print expenses frames
-def expense_print(heading, frame, subtotal):
-    print()
-    print("**** {} Costs ****".format(heading))
-    print(frame)
-    print()
-    print("{} Costs: ${:.2f}".format(heading, subtotal))
-    return ""
+        return [expense_frame, expense_sub]
 
 
 # **** Main routine begins ****
@@ -128,30 +90,14 @@ def expense_print(heading, frame, subtotal):
 # Get product name
 product_name = not_blank("Product name: ", "The product name can't be blank.")
 
-print()
-print("Please enter your variable costs:")
-print()
-# Get variable costs
 variable_expenses = get_expenses("variable")
 variable_frame = variable_expenses[0]
 variable_sub = variable_expenses[1]
 
-print()
-have_fixed = yes_no("Do you have fixed costs (yes/no)? ")
-
-if have_fixed == "yes":
-    fixed_expenses = get_expenses("fixed")
-    fixed_frame = fixed_expenses[0]
-    fixed_sub = fixed_expenses[1]
-else:
-    fixed_sub = 0
-
-
 # **** Printing Area ****
-print()
-print("**** Fun Raising - {} *****".format(product_name))
-print()
-expense_print("Variable", variable_frame, variable_sub)
 
-if have_fixed == "yes":
-    expense_print("Fixed", fixed_frame[['Cost']], fixed_sub)
+print()
+print(variable_frame)
+print()
+
+print("Variable Costs: ${:.2f}".format(variable_sub))
